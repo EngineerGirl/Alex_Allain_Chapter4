a25bb87e934f259142d65c323c5f9d8466de0beb
Sid <siddhata.s.patil@gmail.com>
2016-07-05 10:32:13 -0700

    Create qs3.cpp

---
+++ qs3.cpp
@@ -0,0 +1,16 @@
+/*
+ ==============================================================================================================================
+ Name        : qs3.cpp
+ Author      : Siddhata Patil
+ Copyright   : Copyright (c) Siddhata Patil. Sourcecode rights reserved.
+ Copyright   : Copyright (c) Alex_Allain_Jumping_into_Cpp_Book. Question rights reserved.
+ Question    : Write a small calculator that takes as input one of the four arithmetic operations, the two
+               arguments to those operations, and then prints out the result
+ ==============================================================================================================================
+ */
+ 
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
