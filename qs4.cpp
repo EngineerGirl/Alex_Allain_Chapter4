38a5936fb129398486b9093e19def49fb278f3c8
Sid <siddhata.s.patil@gmail.com>
2016-07-05 10:54:53 -0700

    Update qs4.cpp

--- qs4.cpp
+++ qs4.cpp
@@ -16,3 +16,30 @@
 #include <cstdlib>
 #include <ctime>
 using namespace std;
+
+int main()
+{
+int i =0;
+int username1;
+string password1;
+
+while (i<3)
+	{
+		cout<<"Enter your username: "<<endl;
+		cin >> username1;
+		cout<<"Enter your Password: "<<endl;
+		cin >> password1;
+			if ((username1 == 1 && password1 == "rose") || (username1 == 2 && password1 == "jasmine") || (username1 == 3 && password1 =="lotus"))
+			{
+				cout << "welcome!! \n"; break;
+			}
+			else
+			{i++; 
+			cout << "try again \n";
+			}
+	}
+			if(i==3)
+			cout<< "Sorry...you have exceeded your trials"<<endl;
+system("pause");
+	return 0;	
+}
