3142bd0a411cfd313ae96e8d6e1c168d95972dcf
Sid <siddhata.s.patil@gmail.com>
2016-07-05 10:39:46 -0700

    Update qs3.cpp

--- qs3.cpp
+++ qs3.cpp
@@ -14,3 +14,56 @@
 #include <cstdlib>
 #include <ctime>
 using namespace std;
+
+int add(int n, int m)
+{
+	return n+m;
+}
+int sub(int n, int m)
+{
+	return n-m;
+}
+int mul(int n, int m)
+{
+	return n*m;
+}
+int divi(int n, int m)
+{
+	return (n/m);
+}
+int mod (int n, int m)
+{
+	return n%m;
+
+}
+void calculator(int n1, int n2, char c)
+{
+	if (c=='+')
+		cout << add(n1, n2) <<endl;
+	if (c=='-')
+		cout << sub(n1, n2)<<endl;
+	if (c=='*')
+		cout << mul(n1, n2)<<endl;
+	if (c=='/')
+		cout << divi(n1, n2)<<endl;
+	if (c=='%')
+		cout << mod (n1, n2)<<endl;
+}
+int main()
+{
+	int arg1,arg2;
+	char operation;
+	
+	while(1)
+	{
+	cout<<"Enter your two numbers that you want to calculate:"<<endl;
+	cin>>arg1;
+	cin>>arg2;
+	cout<<"Enter the arithmetic operation you want to carry out: "<<endl;
+	cin>> operation;
+	calculator(arg1,arg2,operation);
+	}
+
+system("pause");
+	return 0;	
+}
