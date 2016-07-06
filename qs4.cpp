5328a90690083531e573f3cdb4316a3c5a766362
Sid <siddhata.s.patil@gmail.com>
2016-07-06 10:33:26 -0700

    Update qs4.cpp

--- qs4.cpp
+++ qs4.cpp
@@ -2,8 +2,8 @@
  ==============================================================================================================================
  Name        : qs4.cpp
  Author      : Siddhata Patil
- Copyright   : Copyright (c) Siddhata Patil. Sourcecode rights reserved.
- Copyright   : Copyright (c) Alex_Allain_Jumping_into_Cpp_Book. Question rights reserved.
+ Copyright   : Copyright ©  Siddhata Patil. Sourcecode rights reserved.
+ Copyright   : Copyright ©  Alex_Allain_Jumping_into_Cpp_Book. Question rights reserved.
  Question    : Expand the password checking program from earlier in this chapter and make it take multiple
                usernames, each with their own password, and ensure that the right username is used for the
                right password. Provide the ability to prompt user's again if the first login attempt failed. Think
