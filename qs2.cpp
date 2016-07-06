9f6da25ac32b1d4ec148ba186ddb0b5ee47c5237
Sid <siddhata.s.patil@gmail.com>
2016-07-06 10:32:56 -0700

    Update qs2.cpp

--- qs2.cpp
+++ qs2.cpp
@@ -2,8 +2,8 @@
  ==============================================================================================================================
  Name        : qs2.cpp
  Author      : Siddhata Patil
- Copyright   : Copyright (c) Siddhata Patil. Sourcecode rights reserved.
- Copyright   : Copyright (c) Alex_Allain_Jumping_into_Cpp_Book. Question rights reserved.
+ Copyright   : Copyright ©  Siddhata Patil. Sourcecode rights reserved.
+ Copyright   : Copyright ©  Alex_Allain_Jumping_into_Cpp_Book. Question rights reserved.
  Question    : Implement a simple "password" system that takes a password in the form of a number. Make it
 so that either of two numbers are valid, but use only one if statement to do the check.
  ==============================================================================================================================
