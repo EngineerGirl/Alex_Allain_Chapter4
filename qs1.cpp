30d76f5bff71e9cd994543af13d5a626cde5b597
Sid <siddhata.s.patil@gmail.com>
2016-07-06 10:32:44 -0700

    Update qs1.cpp

--- qs1.cpp
+++ qs1.cpp
@@ -2,8 +2,8 @@
  ==============================================================================================================================
  Name        : qs1.cpp
  Author      : Siddhata Patil
- Copyright   : Copyright (c) Siddhata Patil. Sourcecode rights reserved.
- Copyright   : Copyright (c) Alex_Allain_Jumping_into_Cpp_Book. Question rights reserved.
+ Copyright   : Copyright ©  Siddhata Patil. Sourcecode rights reserved.
+ Copyright   : Copyright ©  Alex_Allain_Jumping_into_Cpp_Book. Question rights reserved.
  Question    : Ask the user for two users' ages, and indicate who is older; behave differently if both are over
 100.
  ==============================================================================================================================
